1ab374e689bafae55b1c60c41787679e7c4e6842
Jaime Sanchez <josejaime.sanchez@gmail.com>
2014-09-27 11:55:55 +1000

    Adding an action on the menu to see the location on a map

--- app/src/main/java/jjsanchez/org/sunshine/app/MainActivity.java
+++ app/src/main/java/jjsanchez/org/sunshine/app/MainActivity.java
@@ -1,7 +1,9 @@
 package jjsanchez.org.sunshine.app;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -37,6 +39,22 @@ public class MainActivity extends ActionBarActivity {
             startActivity(intent);
             return true;
         }
+        if (id == R.id.action_see_location) {
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse("geo:0,0?").buildUpon().appendQueryParameter("q", getLocationFromPreferences()).build());
+            if (intent.resolveActivity(getPackageManager()) != null) {
+                startActivity(intent);
+                return true;
+            }
+        }
         return super.onOptionsItemSelected(item);
+    }
+
+    private String getLocationFromPreferences() {
+        String locationPreferenceKey = getString(R.string.pref_location_key);
+        String defaultLocation = getString(R.string.pref_location_default);
+        return PreferenceManager
+                .getDefaultSharedPreferences(this)
+                .getString(locationPreferenceKey, defaultLocation);
     }
 }
