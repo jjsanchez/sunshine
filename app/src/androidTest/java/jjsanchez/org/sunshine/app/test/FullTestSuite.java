93c00ac5742c589d9ecc4b36a5089e1ce45e57a8
Jaime Sanchez <josejaime.sanchez@gmail.com>
2014-09-27 18:22:50 +1000

    Adding code for the DB

---
+++ app/src/androidTest/java/jjsanchez/org/sunshine/app/test/FullTestSuite.java
@@ -0,0 +1,13 @@
+package jjsanchez.org.sunshine.app.test;
+
+import android.test.suitebuilder.TestSuiteBuilder;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class FullTestSuite extends TestSuite {
+
+    public static Test suite() {
+        return new TestSuiteBuilder(FullTestSuite.class).includeAllPackagesUnderHere().build();
+    }
+}
